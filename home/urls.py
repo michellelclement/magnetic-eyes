bf00b53ebd7a4c535f07df1eb73a1e8c026341c1
Michelle Clement <michellelclement@gmail.com>
2020-11-02 10:47:52 +0000

    continued styling of application page

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('application/', views.application, name='application'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'home/index.html')
+
+
+def application(request):
+    return render(request, 'home/application.html')
