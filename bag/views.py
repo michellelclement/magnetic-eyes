78bf4febcdbcbb1e2d3067a21de88860c7230ef4
Michelle Clement <michellelclement@gmail.com>
2020-11-05 14:55:29 +0000

    Remove from cart button now working

--- bag/views.py
+++ bag/views.py
@@ -40,21 +40,9 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
-    try:
-        size = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
-        bag = request.session.get('bag', {})
+    bag = request.session.get('bag', {})
 
-        if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
-        else:
-            bag.pop(item_id)
+    bag.pop(item_id)
 
-        request.session['bag'] = bag
-        return HttpResponse(status=200)
-
-    except Exception as e:
-        return HttpResponse(status=500)
+    request.session['bag'] = bag
+    return HttpResponse(status=200)
