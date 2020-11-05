33df070120d603ab8537c03aec6cdebe70f49ac6
Michelle Clement <michellelclement@gmail.com>
2020-11-05 14:35:36 +0000

    Ability to edit quantity in bag now correct and working

--- bag/views.py
+++ bag/views.py
@@ -25,26 +25,16 @@ def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
-    size = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    if size:
-        if quantity > 0:
-            bag[item_id]['items_by_size'][size] = quantity
-        else:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
-                bag.pop(item_id)
+    if quantity > 0:
+        bag[item_id] = quantity
     else:
-        if quantity > 0:
-            bag[item_id] = quantity
-        else:
-            bag.pop(item_id)
+        bag.pop(item_id)
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
 
 
 def remove_from_bag(request, item_id):
