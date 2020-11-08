8589f023165feb5085a8fe2c6fd435b4bc775ae1
Michelle Clement <michellelclement@gmail.com>
2020-11-08 13:42:19 +0000

    Adding Stripe listener. Error to fix on testing card

--- checkout/views.py
+++ checkout/views.py
@@ -29,11 +29,17 @@ def checkout(request):
     print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
+
