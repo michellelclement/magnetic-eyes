dec6f0caecc00f08f993ec4157c5682fe2349118
Michelle Clement <michellelclement@gmail.com>
2020-11-09 09:44:53 +0000

    added checkout success url  and updated imports for views.

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from .models import OrderLineItem
+from .models import Order, OrderLineItem
 from products.models import Product
 from bag.contexts import bag_contents
 
