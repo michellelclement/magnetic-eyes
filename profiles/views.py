60f67d1f036686fa0ac462cb3ae6b517b7ecd162
Michelle Clement <michellelclement@gmail.com>
2020-11-10 18:55:37 +0000

    added profile.html + URLS

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+# Display User Profile
+def profile(request):
+    template = 'profiles/profile.html'
+    context = {}
+
+    return render(request, template, context)
