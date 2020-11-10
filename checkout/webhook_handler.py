b5666f1a107fdd566bfc82ff533d920a942f4ccf
Michelle Clement <michellelclement@gmail.com>
2020-11-10 09:51:16 +0000

    added webhooks.py file

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -9,5 +9,17 @@ class StripeWH_Handler:
     # Handle unknown/generic/unexpected webhook event
     def handle_event(self, event):
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    # Handle payment intent succeeded webhook from stripe
+    def handle_payment_intent_succeeded(self, event):
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    # Handle payment intent failed webhook from stripe
+    def handle_payment_intent_failed(self, event):
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
---
+++ checkout/webhooks.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from checkout.webhook_handler import StripeWH_Handler
+
+import stripe
+
+
+def webhook (request):
+    # Setup
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+        payload, sig_header, wh_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+    except Exception as e:
+        return HttpResponse(content=e, status=400)
