d0631994940183592d80c43258102d573ea37b57
Michelle Clement <michellelclement@gmail.com>
2020-11-11 16:53:44 +0000

    Adding categories to product pages and adding category views

--- products/views.py
+++ products/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
 
     products = Product.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
