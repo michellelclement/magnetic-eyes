2fa319105dd3dce399af7b08f78dfd9ca981699b
Michelle Clement <michellelclement@gmail.com>
2020-11-11 20:03:54 +0000

    Corrected issue with noimage.png. adding products with or without an image

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
 from .models import Product, Category
 
 from .forms import ProductForm
@@ -36,7 +37,17 @@ def product_detail(request, product_id):
 
 # Superuser to add product to store
 def add_product(request):
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Opps! Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
--- profiles/views.py
+++ profiles/views.py
@@ -14,9 +14,11 @@ def profile(request):
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+            messages.success(request, 'Yey! Your profile has been updated successfully')
+        else:
+            messages.error(request, 'Oh no! Your profile failed to upload. Please ensure the form is correctly filled out and valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
