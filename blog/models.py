5287bd4e1d916dbd6739d921899626843f65eab6
Michelle Clement <michellelclement@gmail.com>
2020-11-13 15:45:04 +0000

    adding images to blog feed and some styling to blog page

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,8 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     class Meta:
         ordering = ['-created_on']
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 
 
 def all_posts(request):
-    post_list = Post.objects.filter(status=1)
+    post_list = Post.objects.filter(status=1).order_by('-created_on')
     template = "blog/blog.html"
     context = {
         'post_list': post_list
@@ -14,4 +14,6 @@ def all_posts(request):
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'blog/post_detail.html'
+    template_name = 'post_detail.html'
+
+
