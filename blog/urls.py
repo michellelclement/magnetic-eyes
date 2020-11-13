2f0d4240be49e6ef61c02454d543be6033d21316
Michelle Clement <michellelclement@gmail.com>
2020-11-13 14:25:34 +0000

    Correcting blog views and file formatting

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('blog/', views.PostList.as_view(), name='blog'),
+    path('', views.all_posts, name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
+from django.shortcuts import render
 from django.views import generic
 from .models import Post
+
+
+def all_posts(request):
+    post_list = Post.objects.filter(status=1)
+    template = "blog/blog.html"
+    context = {
+        'post_list': post_list
+    }
+    return render(request, template, context)
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
