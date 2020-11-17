faad098a1bec2bebb6cdf0fafeba9202a28a4e05
Michelle Clement <michellelclement@gmail.com>
2020-11-17 16:32:51 +0000

    Starting the creation of favourites.html

--- magnetic_eyes/urls.py
+++ magnetic_eyes/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
     path('sendemail/', include('sendemail.urls')),
+    path('favourites/', include('favourites.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
