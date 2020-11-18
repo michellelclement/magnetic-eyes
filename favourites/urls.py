9654b8d08c762fd915a05ad45507040e582249f2
Michelle Clement <michellelclement@gmail.com>
2020-11-18 09:23:54 +0000

    Working on add to favourites button. Still have error to fix

--- favourites/urls.py
+++ favourites/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.favourites, name='favourites')
+    path('', views.favourites, name='favourites'),
+    path('favourites/<item_id>/', views.add_to_favourites, name='add_to_favourites')
 ]
--- favourites/views.py
+++ favourites/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
 
 def favourites(request):
 
     return render(request, 'favourites/favourites.html')
+
+
+# Add items to Favourites
+def add_to_favourites(request, item_id):
+
+    redirect_url = request.POST.get('redirect_url')
+    favourites = request.session.get('favourites', {})
+
+    favourites[item_id] = favourites
+
+    request.session['favourites'] = favourites
+    print(request.session['favourites'])
+    return redirect(redirect_url)
