b6663596afcded5beb42dbb971c707778f0165d7
Michelle Clement <michellelclement@gmail.com>
2020-11-19 10:06:12 +0000

    Initial stages of deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn magnetic-eyes.wsgi:application
--- magnetic_eyes/settings.py
+++ magnetic_eyes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -114,12 +115,18 @@ WSGI_APPLICATION = 'magnetic_eyes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
