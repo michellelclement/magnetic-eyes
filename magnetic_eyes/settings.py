c6cc2eb77c02fbe835fb652382a0bca145a698ac
Michelle Clement <michellelclement@gmail.com>
2020-11-19 10:54:16 +0000

    Updating local host setting

--- magnetic_eyes/settings.py
+++ magnetic_eyes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['michellelclement-magneticy-eyes.herokuapp.com', 'local host']
+ALLOWED_HOSTS = ['michellelclement-magneticy-eyes.herokuapp.com', 'localhost']
 
 
 # Application definition
