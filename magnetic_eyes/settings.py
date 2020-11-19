7c7eab312373b11bd44de8f7e358744b1af59112
Michelle Clement <michellelclement@gmail.com>
2020-11-19 12:19:27 +0000

    Adjusting allowed hosts in settings.py for deployment

--- magnetic_eyes/settings.py
+++ magnetic_eyes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['michellelclement-magneticy-eyes.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['magnetic-eyes.herokuapp.com', 'localhost']
 
 
 # Application definition
