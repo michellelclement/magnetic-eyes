0bbbca8877630df403ecbebae20956a3be27d58a
Michelle Clement <michellelclement@gmail.com>
2020-11-20 19:31:23 +0000

    updating sender email

--- sendemail/views.py
+++ sendemail/views.py
@@ -14,7 +14,7 @@ def contactView(request):
             from_email = form.cleaned_data['from_email']
             message = form.cleaned_data['message']
             try:
-                send_mail(subject, message, from_email, ['magneticeyeslashes@gmail.com'])
+                send_mail(subject, message, from_email, ['michellelclement@gmail.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
