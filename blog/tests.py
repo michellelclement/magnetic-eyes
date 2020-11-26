4c970ee8ef2a6d37fcf1be6df38deaddc75692ab
Michelle Clement <michellelclement@gmail.com>
2020-11-26 16:45:35 +0000

    Added form auto testing for blog comments

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .forms import CommentForm
 
-# Create your tests here.
+
+class TestCommentForm(TestCase):
+
+    def test_name_is_required(self):
+        form = CommentForm({'name': ''})
+        self.assertFalse(form.is_valid())
+
+    def test_email_is_required(self):
+        form = CommentForm({'email': ''})
+        self.assertFalse(form.is_valid())
+
+    def test_comment_is_required(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
