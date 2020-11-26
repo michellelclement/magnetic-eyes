207d5a4aeec16eeeea7bbe6386cd2e1f91b971bd
Michelle Clement <michellelclement@gmail.com>
2020-11-26 16:30:21 +0000

    Adjusting code after checking with linter

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,31 +6,31 @@
     As well as the Boutique Ado Project from Code Institue Full Stack Frameworks
 */
 
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $("#id_stripe_public_key").text().slice(1, -1);
+var clientSecret = $("#id_client_secret").text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
-        color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
+        color: "#000",
+        fontFamily: "Helvetica Neue, Helvetica, sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+            color: "#aab7c4"
         }
     },
     invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
+        color: "#dc3545",
+        iconColor: "#dc3545"
     }
 };
-var card = elements.create('card', {style: style});
-card.mount('#card-element');
+var card = elements.create("card");
+card.mount("#card-element");
 
 // For validation errors on card element
-card.addEventListener('change', function (event) {
-    var errorDiv = document.getElementById('card-errors');
+card.addEventListener("change", function (event) {
+    var errorDiv = document.getElementById("card-errors");
     if (event.error) {
         var html = `
             <span class="icon" role="alert">
@@ -40,29 +40,29 @@ card.addEventListener('change', function (event) {
         `;
         $(errorDiv).html(html);
     } else {
-        errorDiv.textContent = '';
+        errorDiv.textContent = "";
     }
 });
 
 // Handle form submit
-var form = document.getElementById('payment-form');
+var form = document.getElementById("payment-form");
 
-form.addEventListener('submit', function(ev) {
+form.addEventListener("submit", function(ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true});
-    $('#submit-button').attr('disabled', true);
-    $('#payment-form').fadeToggle(100);
-    $('#loading-overlay').fadeToggle(100);
+    card.update({ "disabled": true});
+    $("#submit-button").attr("disabled", true);
+    $("#payment-form").fadeToggle(100);
+    $("#loading-overlay").fadeToggle(100);
 
-    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var saveInfo = Boolean($("#id-save-info").attr("checked"));
     // From using {% csrf_token %} in the form
-    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var csrfToken = $("input[name='csrfmiddlewaretoken']").val();
     var postData = {
-        'csrfmiddlewaretoken': csrfToken,
-        'client_secret': clientSecret,
-        'save_info': saveInfo,
+        "csrfmiddlewaretoken": csrfToken,
+        "client_secret": clientSecret,
+        "save_info": saveInfo,
     };
-    var url = '/checkout/cache_checkout_data/';
+    var url = "/checkout/cache_checkout_data/";
 
     $.post(url, postData).done(function() {
         stripe.confirmCardPayment(clientSecret, {
@@ -95,19 +95,19 @@ form.addEventListener('submit', function(ev) {
             },
     }).then(function(result) {
             if (result.error) {
-                var errorDiv = document.getElementById('card-errors');
+                var errorDiv = document.getElementById("card-errors");
                 var html = `
                     <span class="icon" role="alert">
                     <i class="fas fa-times"></i>
                     </span>
                     <span>${result.error.message}</span>`;
                 $(errorDiv).html(html);
-                $('#payment-form').fadeToggle(100);
-                $('#loading-overlay').fadeToggle(100);
-                card.update({ 'disabled': false});
-                $('#submit-button').attr('disabled', false);
+                $("#payment-form").fadeToggle(100);
+                $("#loading-overlay").fadeToggle(100);
+                card.update({ "disabled": false});
+                $("#submit-button").attr("disabled", false);
             } else {
-                if (result.paymentIntent.status === 'succeeded') {
+                if (result.paymentIntent.status === "succeeded") {
                     form.submit();
                 }
             }
