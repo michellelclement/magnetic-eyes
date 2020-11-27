20574cacc9c3c5f4fe94b41ad05b2e168aaf0dee
Michelle Clement <michellelclement@gmail.com>
2020-11-27 14:03:59 +0000

    Updating blog pagination

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def all_posts(request):
     post_list = Post.objects.filter(status=1).order_by('-created_on')
-    paginator = Paginator(post_list, 5)  # 5 posts in each page
+    paginator = Paginator(post_list, 4)  # 4 posts maximum on each page
     page = request.GET.get('page')
     try:
         post_list = paginator.page(page)
