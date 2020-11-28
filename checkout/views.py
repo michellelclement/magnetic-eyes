6214a05e9b559119dcec5d7ee78e5d9c642ef03b
Michelle Clement <michellelclement@gmail.com>
2020-11-28 10:23:44 +0000

    Order number removed from success message

--- checkout/views.py
+++ checkout/views.py
@@ -158,8 +158,7 @@ def checkout_success(request, order_number):
                 user_profile_form.save()
 
     messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        A confirmation email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
