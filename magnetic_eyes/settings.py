21d480cfee21eb2ee61097f99a531853a7c00ce0
Michelle Clement <michellelclement@gmail.com>
2020-11-29 19:17:55 +0000

    Correct bootstrap and css issue for toasts and update button on password reset

--- magnetic_eyes/settings.py
+++ magnetic_eyes/settings.py
@@ -23,6 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEVELOPMENT')
