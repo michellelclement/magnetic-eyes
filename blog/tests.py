7f16a17488f2643ed5ed017a1bf328b44510e03a
Michelle Clement <michellelclement@gmail.com>
2020-12-08 11:47:32 +0000

    Correcting code error caused by prettier

--- blog/tests.py
+++ blog/tests.py
@@ -1,17 +1,1 @@
 from django.test import TestCase
-from .forms import CommentForm
-
-
-class TestCommentForm(TestCase):
-
-    def test_name_is_required(self):
-        form = CommentForm({'name': ''})
-        self.assertFalse(form.is_valid())
-
-    def test_email_is_required(self):
-        form = CommentForm({'email': ''})
-        self.assertFalse(form.is_valid())
-
-    def test_comment_is_required(self):
-        form = CommentForm({'body': ''})
-        self.assertFalse(form.is_valid())
