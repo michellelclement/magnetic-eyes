f103d2c550443a02bab0f510d201a72776a9cbca
Michelle Clement <michellelclement@gmail.com>
2020-12-09 13:43:46 +0000

    Trying test code again. No errors this time for commentform

--- blog/tests.py
+++ blog/tests.py
@@ -1,1 +1,17 @@
 from django.test import TestCase
+from blog.forms import CommentForm
+
+
+class TestCommentForm(TestCase):
+
+    def test_name_is_required(self):
+        form = CommentForm({'name': 'Test name'})
+        self.assertFalse(form.is_valid())
+
+    def test_email_is_required(self):
+        form = CommentForm({'email': ''})
+        self.assertFalse(form.is_valid())
+
+    def test_comment_is_required(self):
+        form = CommentForm({'body': ''})
+        self.assertFalse(form.is_valid())
