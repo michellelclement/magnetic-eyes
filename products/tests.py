d4aae1b5310bdd8ca3e85dc0454526494747cbb8
Michelle Clement <michellelclement@gmail.com>
2020-12-09 14:26:06 +0000

    Using code for other tests, added product form tests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from products.forms import ProductForm
 
-# Create your tests here.
+
+class TestProductForm(TestCase):
+
+    def test_sku_is_not_required(self):
+        form = ProductForm({'name': 'Test Name'
+                            'description:' 'Test Description'
+                            'price:' '£9.99'})
+        self.assertFalse(form.is_valid())
+
+    def test_product_name_is_required(self):
+        form = ProductForm({'name': 'Test name'})
+        self.assertFalse(form.is_valid())
+
+    def test_description_is_required(self):
+        form = ProductForm({'description': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('description', form.errors.keys())
+        self.assertEqual(form.errors['description'][0], 'This field is required.')
+
+    def test_price_is_required(self):
+        form = ProductForm({'price': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('price', form.errors.keys())
+        self.assertEqual(form.errors['price'][0], 'This field is required.')
